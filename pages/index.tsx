ed15f5eb8b8348e2469de336a17a0626f3b049b3
Hasan Özacar <hozacar@ARC607MAC.local>
2021-03-16 02:05:48 +0300

    Remove Head for deployment

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,10 +4,6 @@ import styles from '../styles/Home.module.css';
 
 const Home: React.FC = () => (
   <div className={styles.container}>
-    <Head>
-      <title>Create Next App</title>
-      <link rel="icon" href="/favicon.ico" />
-    </Head>
 
     <main className={styles.main}>
       <h1 className={styles.title}>
